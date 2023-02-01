1b8584a3eda8a09d899abe924ad4854a0f027c4e
Lucca <luccazvps@gmail.com>
2023-02-01 10:34:08 -0300

    feat: add client and architect types

---
+++ src/domain/architect.ts
@@ -0,0 +1,11 @@
+export interface Architect {
+  name: string;
+  email: string;
+  address: string;
+  number: string;
+  instagram: string;
+  observations: string;
+  sampleDate: string;
+  catalog: string;
+  bankInfo: string;
+}
---
+++ src/domain/client.ts
@@ -0,0 +1,10 @@
+export interface Client {
+  name: string;
+  email: string;
+  addres: string;
+  number: string;
+  instagram: string;
+  indication: string;
+  aquisitions: string;
+  observations: string;
+}
