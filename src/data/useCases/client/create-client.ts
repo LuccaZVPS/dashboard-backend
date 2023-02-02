6ca5a3ce96312baf3e546279a92f8613cc0fcaba
Lucca <luccazvps@gmail.com>
2023-02-02 18:08:39 -0300

    feat: add create client use case

---
+++ src/data/useCases/client/create-client.ts
@@ -0,0 +1,11 @@
+import { Client } from "../../../domain/client";
+import { CreateClient as CreateClientType } from "../../../domain/useCases/create-client";
+import { CreateClientDTO } from "../../../presentation/controllers/client/DTOs/create-client";
+import { CreateClientRepository } from "../../protocols/client/create-client-repository";
+export class CreateClient implements CreateClientType {
+  constructor(private readonly createRepository: CreateClientRepository) {}
+  async create(createClientDTO: CreateClientDTO): Promise<Client> {
+    const client = await this.createRepository.create(createClientDTO);
+    return client;
+  }
+}
