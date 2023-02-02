2baab509e2809c3e1c7109ab07a58aeab9e9feb5
Lucca <luccazvps@gmail.com>
2023-02-02 12:02:58 -0300

    feat: add ValidatorDTO

--- src/infra/validators/class-validator.ts
+++ src/infra/validators/class-validator.ts
@@ -1,6 +1,25 @@
-import { Validate } from "class-validator";
-export class Validator {
+import { validate as validateDTO } from "class-validator";
+import { DTOValidator } from "../../presentation/protocols/DTO-validator";
+export const Validate = async (classModel: any) => {
+  return await validateDTO(classModel);
+};
+class Validator implements DTOValidator {
   async validate(classModel: any) {
-    const isCorrect = Validate(classModel);
+    const isValid = await validators.Validate(classModel);
+    if (isValid.length === 0) {
+      return { errors: "" };
+    }
+
+    const errors = JSON.stringify(
+      isValid.map((errors) => {
+        return {
+          field: errors.property,
+          errors: errors.constraints,
+        };
+      })
+    );
+
+    return { errors };
   }
 }
+export const validators = { Validator, Validate };
