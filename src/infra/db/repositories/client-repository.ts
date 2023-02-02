42475956cfa2c644017dff236abf2bbf9679d798
Lucca <luccazvps@gmail.com>
2023-02-02 20:20:06 -0300

    feat: add create method to client repository

---
+++ src/infra/db/repositories/client-repository.ts
@@ -0,0 +1,18 @@
+import { CreateClientRepository } from "../../../data/protocols/client/create-client-repository";
+import { GetClientsRepository } from "../../../data/protocols/client/get-clients-repository";
+import { Client } from "../../../domain/client";
+import { CreateClientDTO } from "../../../presentation/controllers/client/DTOs/create-client";
+import { clientModel } from "../schemas/client.schema";
+
+export class ClientRepository implements CreateClientRepository {
+  async create(DTO: CreateClientDTO): Promise<Client> {
+    const client = await clientModel.create({
+      ...DTO,
+    });
+
+    if (client._id) {
+      return client as unknown as Client;
+    }
+    throw new Error();
+  }
+}
