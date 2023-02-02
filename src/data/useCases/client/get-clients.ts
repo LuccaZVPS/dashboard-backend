c08ad659c711b9b18e91e081d8be96a9cfbc58c3
Lucca <luccazvps@gmail.com>
2023-02-02 18:21:17 -0300

    feat: add getClients use case

---
+++ src/data/useCases/client/get-clients.ts
@@ -0,0 +1,10 @@
+import { Client } from "../../../domain/client";
+import { GetClients as GetClientsType } from "../../../domain/useCases/get-clients";
+import { GetClientsRepository } from "../../protocols/client/get-clients-repository";
+export class GetClients implements GetClientsType {
+  constructor(private readonly getClientsRepository: GetClientsRepository) {}
+  async get(): Promise<Client[]> {
+    const clients = await this.getClientsRepository.get();
+    return clients;
+  }
+}
