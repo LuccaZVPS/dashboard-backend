19a6e45318762ca1e08e951965fbf9ddadf606dd
Lucca <luccazvps@gmail.com>
2023-02-02 20:25:09 -0300

    test: add tests for get method in client repository

--- tests/infra/repositories/client-repository.spec.ts
+++ tests/infra/repositories/client-repository.spec.ts
@@ -22,6 +22,9 @@ describe("Client repository", () => {
   afterAll(() => {
     mongoHelper.close();
   });
+  afterEach(async () => {
+    await clientModel.deleteMany();
+  });
   const makeSut = () => {
     return {
       sut: new ClientRepository(),
@@ -40,6 +43,25 @@ describe("Client repository", () => {
     });
     expect(async () => {
       await sut.create(DTOMock);
+    }).rejects.toThrow(new Error());
+  });
+
+  test("get method should list all clients ", async () => {
+    const { sut } = makeSut();
+    await clientModel.create(DTOMock);
+    await clientModel.create(DTOMock);
+    const clients = await sut.get();
+    expect(clients.length).toBe(2);
+    expect(clients[0]._id).toBeTruthy();
+    expect(clients[1]._id).toBeTruthy();
+  });
+  test("get method should throws if mongo throws", async () => {
+    const { sut } = makeSut();
+    jest.spyOn(clientModel, "find").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    expect(async () => {
+      await sut.get();
     }).rejects.toThrow(new Error());
   });
 });
