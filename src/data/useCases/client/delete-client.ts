215302d81cf5d41694d9b46409864a8e6b2ecd11
Lucca <luccazvps@gmail.com>
2023-02-02 18:45:38 -0300

    feat: add deleteAccount use case

---
+++ src/data/useCases/client/delete-client.ts
@@ -0,0 +1,13 @@
+import { DeleteClient as DeleteClientType } from "../../../domain/useCases/delete-client";
+import { DeleteClientRepository } from "../../protocols/client/delete-client-repository";
+export class DeleteClient implements DeleteClientType {
+  constructor(
+    private readonly deleteClientRepository: DeleteClientRepository
+  ) {}
+  async delete(_id: string): Promise<void> {
+    const isDeleted = await this.deleteClientRepository.delete(_id);
+    if (!isDeleted) {
+      throw new Error();
+    }
+  }
+}
