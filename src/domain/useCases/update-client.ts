eac2c55f09c1e041f68ae4709c2676ae6fa257ca
Lucca <luccazvps@gmail.com>
2023-02-02 08:35:33 -0300

    refactor: change UpdateClient params

--- src/domain/useCases/update-client.ts
+++ src/domain/useCases/update-client.ts
@@ -1,7 +1,8 @@
 import { CreateClientDTO } from "../../presentation/controllers/client/DTOs/create-client";
+import { UpdateClientDTO } from "../../presentation/controllers/client/DTOs/update-client";
 import { Client } from "../client";
 import { CreateClient } from "./create-client";
 
 export interface UpdateClient {
-  update: (client: CreateClientDTO) => Promise<Client>;
+  update: (client: UpdateClientDTO) => Promise<Client>;
 }
