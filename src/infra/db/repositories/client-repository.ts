71f8f4c878f6a0baba899ab5a7ff0e35200807e0
Lucca <luccazvps@gmail.com>
2023-02-02 20:50:05 -0300

    feat: add update method to client repository

--- src/infra/db/repositories/client-repository.ts
+++ src/infra/db/repositories/client-repository.ts
@@ -1,15 +1,18 @@
 import { CreateClientRepository } from "../../../data/protocols/client/create-client-repository";
 import { DeleteClientRepository } from "../../../data/protocols/client/delete-client-repository";
 import { GetClientsRepository } from "../../../data/protocols/client/get-clients-repository";
+import { UpdateClientRepository } from "../../../data/protocols/client/update-client-repository";
 import { Client } from "../../../domain/client";
 import { CreateClientDTO } from "../../../presentation/controllers/client/DTOs/create-client";
+import { UpdateClientDTO } from "../../../presentation/controllers/client/DTOs/update-client";
 import { clientModel } from "../schemas/client.schema";
 
 export class ClientRepository
   implements
     CreateClientRepository,
     GetClientsRepository,
-    DeleteClientRepository
+    DeleteClientRepository,
+    UpdateClientRepository
 {
   async create(DTO: CreateClientDTO): Promise<Client> {
     const client = await clientModel.create({
@@ -31,5 +34,17 @@ export class ClientRepository
       throw new Error();
     }
     return true;
+  }
+  async update(client: UpdateClientDTO): Promise<Client> {
+    const updatedClient = await clientModel.findOneAndUpdate(
+      { _id: client._id },
+      {
+        $set: {
+          ...client,
+        },
+      },
+      { new: true }
+    );
+    return updatedClient as unknown as Client;
   }
 }
