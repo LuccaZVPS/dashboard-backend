b16b4ea4faf166a19880629a6ba9a5d9947d4da9
Lucca <luccazvps@gmail.com>
2023-02-03 11:13:12 -0300

    feat: open connection to mongoDB

--- src/infra/apollo/index.ts
+++ src/infra/apollo/index.ts
@@ -1,6 +1,12 @@
 import { ApolloServer } from "apollo-server";
 import { resolvers, typeDefs } from "./rootTypes";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { config } from "dotenv";
+import { mongoHelper } from "../db/connection";
+config();
+mongoHelper.connect("mongodb://127.0.0.1:27017/test").then(() => {
+  console.log("Mongo connected");
+});
 export const apolloServer = new ApolloServer({
   resolvers,
   typeDefs,
