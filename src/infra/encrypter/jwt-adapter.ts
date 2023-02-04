ba45c9ba7d877ea69c37cadeddb747def1b57236
Lucca <luccazvps@gmail.com>
2023-02-04 16:18:37 -0300

    feat: add decrypt jwt method

--- src/infra/encrypter/jwt-adapter.ts
+++ src/infra/encrypter/jwt-adapter.ts
@@ -1,12 +1,17 @@
 import { CreateJwt } from "../../presentation/protocols/create-jwt";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
+import { DecryptJwtTs } from "../../presentation/protocols/decrypt-jwt";
 export const jwt = {
   sign,
+  verify,
 };
-export class JWTAdapter implements CreateJwt {
+export class JWTAdapter implements CreateJwt, DecryptJwtTs {
   async encrypt(_id: string): Promise<string> {
     const token = jwt.sign({ _id }, process.env.JWT_SECRET);
-    console.log(token);
+    return token;
+  }
+  async decrypt(jwtString: string): Promise<any> {
+    const token = jwt.verify(jwtString, process.env.JWT_SECRET);
     return token;
   }
 }
