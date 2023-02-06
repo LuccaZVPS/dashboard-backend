ab2349b08d76f7633878b7aa20a079cb0318a2ed
Lucca <luccazvps@gmail.com>
2023-02-06 10:53:17 -0300

    feat: ensure deleteArchitectController call validate with correct value

--- src/presentation/controllers/architect/delete-architect.ts
+++ src/presentation/controllers/architect/delete-architect.ts
@@ -1,12 +1,22 @@
 import { AuthenticationError } from "apollo-server-core";
 import { DeleteArchitect } from "../../../domain/useCases/architect/delete-architect";
 import { Context, Contoller, Data } from "../../protocols/controller";
+import { DTOValidator } from "../../protocols/DTO-validator";
+import { DeleteArchitectDTO } from "./DTOs/delete-architect-dto";
 
 export class DeleteArchitectController implements Contoller {
-  constructor(private readonly deleteArchitect: DeleteArchitect) {}
-  async handle(_: any, data: Data, { userId }: Context): Promise<any> {
+  constructor(
+    private readonly deleteArchitect: DeleteArchitect,
+    private readonly validator: DTOValidator
+  ) {}
+  async handle(_: any, { data }: Data, { userId }: Context): Promise<any> {
     if (!userId) {
       throw new AuthenticationError("must be logged in");
     }
+    const deleteArchitectDTO = new DeleteArchitectDTO();
+    if (data?._id) {
+      deleteArchitectDTO._id = data?._id;
+    }
+    const isValidDTO = await this.validator.validate(deleteArchitectDTO);
   }
 }
