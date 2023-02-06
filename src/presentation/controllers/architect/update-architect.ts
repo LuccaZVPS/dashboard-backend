b30794f43645cb7f893186680ba9d2b6460616c3
Lucca <luccazvps@gmail.com>
2023-02-06 11:39:35 -0300

    feat: ensure updateArchitectController call validate method with correct values

--- src/presentation/controllers/architect/update-architect.ts
+++ src/presentation/controllers/architect/update-architect.ts
@@ -3,6 +3,7 @@ import { FindArchitect } from "../../../domain/useCases/architect/find-architect
 import { UpdateArchitect } from "../../../domain/useCases/architect/update-architect";
 import { Context, Contoller, Data } from "../../protocols/controller";
 import { DTOValidator } from "../../protocols/DTO-validator";
+import { UpdateArchitectDTO } from "./DTOs/update-architect";
 
 export class UpdateArchitectController implements Contoller {
   constructor(
@@ -14,5 +15,25 @@ export class UpdateArchitectController implements Contoller {
     if (!userId) {
       throw new AuthenticationError("must be logged in");
     }
+    const fields = [
+      "_id",
+      "name",
+      "email",
+      "address",
+      "number",
+      "instagram",
+      "observations",
+      "sampleDate",
+      "catalog",
+      "bankInfo",
+      "_id",
+    ];
+    const createArchitectDTO = new UpdateArchitectDTO();
+    for (let field of fields) {
+      if (data[field]) {
+        createArchitectDTO[field] = data[field];
+      }
+    }
+    const isValidDTO = await this.validator.validate(createArchitectDTO);
   }
 }
