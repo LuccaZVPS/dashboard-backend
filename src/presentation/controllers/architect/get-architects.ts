665a2f3b45d472d236aebd72745f24a608ef22a5
Lucca <luccazvps@gmail.com>
2023-02-06 10:23:49 -0300

    feat: ensure getArchitectsController throws auth error if no userId is provided by context

---
+++ src/presentation/controllers/architect/get-architects.ts
@@ -0,0 +1,13 @@
+import { AuthenticationError } from "apollo-server-core";
+import { GetArchitecs } from "../../../domain/useCases/architect/get-architects";
+import { Context, Contoller, Data } from "../../protocols/controller";
+import { DTOValidator } from "../../protocols/DTO-validator";
+
+export class GetArchitectsController implements Contoller {
+  constructor(private readonly getArchitects: GetArchitecs) {}
+  async handle(_: any, data: Data, { userId }: Context): Promise<any> {
+    if (!userId) {
+      throw new AuthenticationError("must be logged in");
+    }
+  }
+}
