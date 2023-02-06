78ab48e2db3992c7f705f498da25dbb237d7067f
Lucca <luccazvps@gmail.com>
2023-02-06 11:53:44 -0300

    test: finish updateArchitectController tests

--- tests/presentation/architect/update-architect.spec.ts
+++ tests/presentation/architect/update-architect.spec.ts
@@ -1,4 +1,4 @@
-import { AuthenticationError } from "apollo-server-core";
+import { AuthenticationError, ValidationError } from "apollo-server-core";
 import { Architect } from "../../../src/domain/architect";
 import { FindArchitect } from "../../../src/domain/useCases/architect/find-architect";
 import { UpdateArchitect } from "../../../src/domain/useCases/architect/update-architect";
@@ -41,7 +41,7 @@ describe("UpdateArchitect controller", () => {
   const makeUpdateArchitectStub = () => {
     class UpdateArchitectStub implements UpdateArchitect {
       async update(architect: UpdateArchitectDTO): Promise<Architect> {
-        return updateArchitectMockDTO;
+        return { ...updateArchitectMockDTO, name: "updated_name" };
       }
     }
     return new UpdateArchitectStub();
@@ -77,7 +77,7 @@ describe("UpdateArchitect controller", () => {
     );
     expect(spy).toBeCalledWith({ ...updateArchitectMockDTO });
   });
-  test("should ", () => {
+  test("should throws user input error if validate return errors messages", () => {
     const { sut, validatorStub } = makeSut();
     jest.spyOn(validatorStub, "validate").mockImplementationOnce(async () => {
       return { errors: "any_errors" };
@@ -89,5 +89,77 @@ describe("UpdateArchitect controller", () => {
         { userId: "any" }
       );
     }).rejects.toThrow(new AuthenticationError("any_errors"));
+  });
+  test("should call find method with correct value", async () => {
+    const { sut, findArchitectStub } = makeSut();
+    const spy = jest.spyOn(findArchitectStub, "find");
+    await sut.handle(
+      "",
+      { data: { ...updateArchitectMockDTO } },
+      { userId: "any" }
+    );
+    expect(spy).toHaveBeenCalledWith(updateArchitectMockDTO._id);
+  });
+  test("should return validation error if find method returns void", async () => {
+    const { sut, findArchitectStub } = makeSut();
+    jest.spyOn(findArchitectStub, "find").mockImplementationOnce(async () => {
+      return;
+    });
+
+    expect(async () => {
+      await sut.handle(
+        "",
+        { data: { ...updateArchitectMockDTO } },
+        { userId: "any" }
+      );
+    }).rejects.toThrow(new ValidationError("architect does not exist"));
+  });
+  test("should call update method with correct values", async () => {
+    const { sut, updateArchitectStub } = makeSut();
+    const spy = jest.spyOn(updateArchitectStub, "update");
+    await sut.handle(
+      "",
+      { data: { ...updateArchitectMockDTO } },
+      { userId: "any" }
+    );
+    expect(spy).toHaveBeenCalledWith({ ...updateArchitectMockDTO });
+  });
+  test("should throws if update method throws", () => {
+    const { sut, updateArchitectStub } = makeSut();
+    jest.spyOn(updateArchitectStub, "update").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    expect(async () => {
+      await sut.handle(
+        "",
+        { data: { ...updateArchitectMockDTO } },
+        { userId: "any" }
+      );
+    }).rejects.toThrow();
+  });
+  test("should throws if find method throws", () => {
+    const { sut, findArchitectStub } = makeSut();
+    jest.spyOn(findArchitectStub, "find").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    expect(async () => {
+      await sut.handle(
+        "",
+        { data: { ...updateArchitectMockDTO } },
+        { userId: "any" }
+      );
+    }).rejects.toThrow();
+  });
+  test("should return an updated architect", async () => {
+    const { sut } = makeSut();
+    const updatedArchitect = await sut.handle(
+      "",
+      { data: { ...updateArchitectMockDTO } },
+      { userId: "any" }
+    );
+    expect(updatedArchitect).toEqual({
+      ...updateArchitectMockDTO,
+      name: "updated_name",
+    });
   });
 });
