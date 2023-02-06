042be3203c21137a61241b7f0f8c7046f52288b8
Lucca <luccazvps@gmail.com>
2023-02-06 09:28:49 -0300

    feat: add DTO for create architect controller

---
+++ src/presentation/controllers/architect/DTOs/create-architect-dto.ts
@@ -0,0 +1,34 @@
+import { IsDefined, ValidateIf } from "class-validator";
+
+export class CreateArchitectDTO {
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  name: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  email: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  address: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  number: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  instagram: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  observations: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  sampleDate: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  catalog: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  bankInfo: string;
+  @IsDefined()
+  @ValidateIf((e) => e === "")
+  _id: string;
+}
